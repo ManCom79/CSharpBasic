5ee232d695da868fbad7740ea09652fdf52447dd
Sasha Stamenkovski <prof.menko@gmail.com>
2024-02-29 12:00:07 +0100

    Swap Numbers task solved

---
+++ 02_NumberOperations/NumberOperations/SwapNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            string firstNumberInput = Console.ReadLine();
+
+            Console.WriteLine("Enter the second number: ");
+            string secondNumberInput = Console.ReadLine();
+
+            string tempValueKeeper = firstNumberInput;
+            
+            firstNumberInput = secondNumberInput;
+            secondNumberInput = tempValueKeeper;
+
+            Console.WriteLine($"First number is: {firstNumberInput}");
+            Console.WriteLine($"Second number is: {secondNumberInput}");
+
+        }
+    }
+}
