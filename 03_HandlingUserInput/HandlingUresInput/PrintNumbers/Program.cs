0ca12719e3dcf5168a7ba6191f26bde3dd18dcf6
Sasha Stamenkovski <prof.menko@gmail.com>
2024-03-04 14:15:31 +0100

    PrintNumbers

---
+++ 03_HandlingUserInput/HandlingUresInput/PrintNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace PrintNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a number: ");
+                string firstNumberInput = Console.ReadLine();
+
+                if (int.TryParse(firstNumberInput, out number1))
+                {
+                    for (int i = 1; i <= number1; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    break;
+                }
+
+                Console.WriteLine("Wrong input. Please enter number!");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a number: ");
+                string secondNumberInput = Console.ReadLine();
+
+                if (int.TryParse(secondNumberInput, out number2))
+                {
+                    for (int i = number2; i>= 1; i--)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    break;
+                }
+
+                Console.WriteLine("Wrong input. Please enter number!");
+            }
+        }
+    }
+}
