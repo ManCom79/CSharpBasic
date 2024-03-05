253773aeda16cc6c247445fd910d1c9c9f164ee3
Sasha Stamenkovski <prof.menko@gmail.com>
2024-03-05 20:01:00 +0100

    Sum of Even Numbers in Array

---
+++ 04_Arrays/Arrays/SumOfEvenNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SumOfEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+            int sum = 0;
+            int userInteger;
+            int countEntries = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"Please enter number {countEntries + 1}");
+                string userEntry = Console.ReadLine();
+
+                if(!int.TryParse(userEntry, out userInteger))
+                {
+                    Console.WriteLine("Invalid entry. Please enter a number!");
+                    continue;
+                }
+
+                numbers[countEntries] = userInteger;
+                countEntries++;
+
+                if(countEntries == 6)
+                {
+                    break;
+                }
+            }
+
+            foreach(int number in numbers)
+            {
+                if(number % 2 == 0) {
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine($"The sum of all even numbers in the array is {sum}.");
+        }
+    }
+}
