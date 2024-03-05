b3cdbd39a1337ea6c5fe0c7432c7b7f528ed252d
Sasha Stamenkovski <prof.menko@gmail.com>
2024-03-05 21:04:01 +0100

    Declare different data-types arrays solved

---
+++ 04_Arrays/Arrays/DeclareDiffDataTypesArryas/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DeclareDiffDataTypesArryas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsArray = new string[] { "Seavus", "Academy", "is", "Qinshift", "Academy" };
+            decimal[] decimalsArray = new decimal[] { 1.5m, 3.5m, 5.5m, 7.5m, 9.5m };
+            char[] charsArray = new char[] { 'a', 'b', '\u002B', 'd', 'e' };
+            bool[] boolArray = new bool[] { true, false, true, false, true };
+            int[][] matrix = new int[][] { [1, 2], [3, 4], [5, 6], [7, 8], [9, 10] };
+
+            Console.WriteLine("Place debugger here to check arrays");
+        }
+    }
+}
