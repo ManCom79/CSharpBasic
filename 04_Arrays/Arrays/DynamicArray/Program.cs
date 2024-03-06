fdf928d44d42ce7de4a649267c99c93ec0193136
Sasha Stamenkovski <prof.menko@gmail.com>
2024-03-06 09:34:41 +0100

    Dynamic Array Solution 01

--- 04_Arrays/Arrays/DynamicArray/Program.cs
+++ 04_Arrays/Arrays/DynamicArray/Program.cs
@@ -7,46 +7,45 @@
             string[] names = new string[0];
             string userInput;
 
-            do
+            while (true)
             {
+                Console.WriteLine("Please enter a name: ");
+                string userNameInput = Console.ReadLine();
+
+                Array.Resize(ref names, names.Length + 1);
+                names[names.Length - 1] = userNameInput;
+
                 while (true)
                 {
-                    Console.WriteLine("Please enter a name: ");
-                    string userNameInput = Console.ReadLine();
+                    Console.WriteLine("Would you like to enter another name? (Y/N)");
+                    string userToContinueInput = Console.ReadLine();
+                    userInput = userToContinueInput.ToLower();
 
-                    Array.Resize(ref names, names.Length + 1);
-                    names[names.Length - 1] = userNameInput;
-
-                    while (true)
+                    if (userInput == "y")
                     {
-                        Console.WriteLine("Would you like to enter another name? (Y/N)");
-                        string userToContinueInput = Console.ReadLine();
-                        userInput = userToContinueInput.ToLower();
-
-                        if (userInput == "y")
-                        {
-                            break;
-                        }
-
-                        if (userInput == "n")
-                        {
-                            break;
-                        }
-
-                        if (userInput != "y" || userInput != "n")
-                        {
-                            Console.WriteLine("Please enter Y or N.");
-                            continue;
-                        }
-
-
+                        break;
                     }
 
+                    if (userInput != "y" && userInput != "n")
+                    {
+                        Console.WriteLine("Please enter Y or N.");
+                        continue;
+                    }
+                    if (userInput == "n")
+                    {
+                        break;
+                    }
                 }
-            } while (userInput == "n");
+                if (userInput == "n")
+                {
+                    break;
+                }
+            }
 
-
-
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
