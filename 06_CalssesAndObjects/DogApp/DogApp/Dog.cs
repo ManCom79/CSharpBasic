f15a47308040dc9915991dc1b2f2e3f511eaf446
Sasha Stamenkovski <prof.menko@gmail.com>
2024-03-12 14:03:58 +0100

    Dog  Object from Dog class

---
+++ 06_CalssesAndObjects/DogApp/DogApp/Dog.cs
@@ -0,0 +1,33 @@
+﻿namespace DogApp
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+
+        public Dog(string name, string breed, string color) {
+            Name = name;
+            Breed = breed;
+            Color = color;
+        }
+
+        public string Eat()
+        {
+            string message = $"{Name} is now eating.";
+            return message;
+        }
+
+        public string Play()
+        {
+            string message = $"{Name} is now playing.";
+            return message;
+        }
+
+        public string ChaseTail()
+        {
+            string message = $"{Name} is now chasing it's tail";
+            return message;
+        }
+    }
+}
---
+++ 06_CalssesAndObjects/DogApp/DogApp/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace DogApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+
+            Console.WriteLine($"What is the dog's name?");
+            string userInputDogName = Console.ReadLine();
+
+            Console.WriteLine($"What breed is the dog?");
+            string userInputDogBreed = Console.ReadLine();
+
+            Console.WriteLine($"What color is the dog?");
+            string userInputDogColor = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine($"Please enter number");
+                string userInput = Console.ReadLine();
+
+                if(!int.TryParse(userInput, out userNumber))
+                {
+                    Console.WriteLine("Please enter a number");
+                    continue;
+                }
+                break;  
+            }
+
+            Dog dog = new Dog(userInputDogName, userInputDogBreed, userInputDogColor);
+
+            Console.WriteLine($"{dog.Name} is a dog of {dog.Breed} breed. {dog.Name} is {dog.Color}.");
+
+            switch (userNumber)
+            {
+                case 1:
+                    {
+                        Console.WriteLine(dog.Eat());
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine(dog.Play());
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine(dog.ChaseTail());
+                        break;
+                    }
+            }
+        }
+    }
+}
