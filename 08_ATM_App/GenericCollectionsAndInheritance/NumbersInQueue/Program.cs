88eb1b5ce9b499e48c21bbf71627703e6f62843b
Sasha Stamenkovski <prof.menko@gmail.com>
2024-03-19 13:13:57 +0100

    NUmbersInQueue task solved

---
+++ 08_ATM_App/GenericCollectionsAndInheritance/NumbersInQueue/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace NumbersInQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbersQueue = new Queue<int>();
+
+            while (true)
+            {
+                bool enterNewNumber = false;
+
+                Console.Write("Please enter a number: ");
+                string userInput = Console.ReadLine();
+
+                if (!int.TryParse(userInput, out int enteredNumber))
+                {
+                    Console.WriteLine("Invalid Input!");
+                    continue;
+                } else
+                {
+                    numbersQueue.Enqueue(enteredNumber);
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Would you like to enter another number (y/n)?");
+                    string enterNumberAgain = Console.ReadLine();
+
+                    if (enterNumberAgain.ToLower() == "y")
+                    {
+                        enterNewNumber = true;
+                        break;
+                    };
+
+                    if (enterNumberAgain.ToLower() == "n")
+                    {
+                        break;
+                    };
+
+                    if (enterNumberAgain.ToLower() != "y" && enterNumberAgain.ToLower() != "n")
+                    {
+                        Console.WriteLine("Please enter \"y\" or \"n\"");
+                        continue;
+                    }
+                }
+
+                if (enterNewNumber == true)
+                {
+                    continue;
+                } else
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine("\nThe numbers in the queue are entered in this order:");
+            foreach (int number in numbersQueue)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
